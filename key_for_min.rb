16e25c5d41ef8bc088801b5f016016083ff12a62
Ally Morson <ally.morson@gmail.com>
2019-10-04 23:10:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(ikea)
-ikea = {chair: 25 , table: 85 , mattress: 450}
-
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
